19ab531ddffa0368522693b1200c6e32f5159bf0
Investigador ETSIT <samuel.rey.escudero@gmail.com>
2016-07-13 12:58:12 +0200

    Adding tutorial2 and new example to tutorial3

--- Tutorial2/tutorial_two.py
+++ Tutorial2/tutorial_two.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Top Block
-# Generated: Wed Jul 13 12:57:06 2016
+# Title: Tutorial Two
+# Generated: Wed Jun 29 13:01:00 2016
 ##################################################
 
 if __name__ == '__main__':
@@ -17,27 +17,25 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
+from PyQt4.QtCore import QObject, pyqtSlot
 from gnuradio import analog
-from gnuradio import blocks
-from gnuradio import digital
+from gnuradio import audio
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
-import numpy
-import python_tutorial
 import sip
 import sys
 
 
-class top_block(gr.top_block, Qt.QWidget):
+class tutorial_two(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Top Block")
+        gr.top_block.__init__(self, "Tutorial Two")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Top Block")
+        self.setWindowTitle("Tutorial Two")
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
         except:
@@ -54,22 +52,35 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "tutorial_two")
         self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 48e3
+        self.audio_rate = audio_rate = 48000
 
         ##################################################
         # Blocks
         ##################################################
+        self._samp_rate_options = (48e3, 24e3, 160e3, )
+        self._samp_rate_labels = ("48K", "24K", "16K", )
+        self._samp_rate_tool_bar = Qt.QToolBar(self)
+        self._samp_rate_tool_bar.addWidget(Qt.QLabel("Signal Sampling Rate"+": "))
+        self._samp_rate_combo_box = Qt.QComboBox()
+        self._samp_rate_tool_bar.addWidget(self._samp_rate_combo_box)
+        for label in self._samp_rate_labels: self._samp_rate_combo_box.addItem(label)
+        self._samp_rate_callback = lambda i: Qt.QMetaObject.invokeMethod(self._samp_rate_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._samp_rate_options.index(i)))
+        self._samp_rate_callback(self.samp_rate)
+        self._samp_rate_combo_box.currentIndexChanged.connect(
+        	lambda i: self.set_samp_rate(self._samp_rate_options[i]))
+        self.top_layout.addWidget(self._samp_rate_tool_bar)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
         	1024, #size
-        	samp_rate, #samp_rate
+        	audio_rate, #samp_rate
         	"", #name
-        	1 #number of inputs
+        	2 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
@@ -98,7 +109,7 @@ class top_block(gr.top_block, Qt.QWidget):
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
                   1.0, 1.0, 1.0, 1.0, 1.0]
         
-        for i in xrange(1):
+        for i in xrange(2):
             if len(labels[i]) == 0:
                 self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -111,68 +122,63 @@ class top_block(gr.top_block, Qt.QWidget):
         
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
         	1024, #size
-        	"", #name
-        	1 #number of inputs
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	audio_rate, #bw
+        	"QT GUI Plot", #name
+        	2 #number of inputs
         )
-        self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
-        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
-        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0.enable_autoscale(False)
-        self.qtgui_const_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
         
         if not True:
-          self.qtgui_const_sink_x_0.disable_legend()
+          self.qtgui_freq_sink_x_0.disable_legend()
+        
+        if "float" == "float" or "float" == "msg_float":
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
         
         labels = ["", "", "", "", "",
                   "", "", "", "", ""]
         widths = [1, 1, 1, 1, 1,
                   1, 1, 1, 1, 1]
-        colors = ["blue", "red", "red", "red", "red",
-                  "red", "red", "red", "red", "red"]
-        styles = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        markers = [0, 0, 0, 0, 0,
-                   0, 0, 0, 0, 0]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
                   1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
+        for i in xrange(2):
             if len(labels[i]) == 0:
-                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
         
-        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.python_tutorial_qpsk_demod_py_cb_0 = python_tutorial.qpsk_demod_py_cb(1)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_ic((1+1j, 1-1j, -1+1j, -1-1j), 1)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
-        self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.analog_random_source_x_0 = blocks.vector_source_i(map(int, numpy.random.randint(0, 4, 10000)), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, .1, 0)
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.audio_sink_0 = audio.sink(audio_rate, "", True)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, 350, 0.1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, 440, 0.1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))    
-        self.connect((self.analog_random_source_x_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))    
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))    
-        self.connect((self.blocks_add_xx_0, 0), (self.python_tutorial_qpsk_demod_py_cb_0, 0))    
-        self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))    
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_const_sink_x_0, 0))    
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0, 0))    
-        self.connect((self.python_tutorial_qpsk_demod_py_cb_0, 0), (self.blocks_char_to_float_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.audio_sink_0, 1))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.qtgui_freq_sink_x_0, 1))    
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.qtgui_time_sink_x_0, 1))    
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings = Qt.QSettings("GNU Radio", "tutorial_two")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -182,11 +188,20 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self._samp_rate_callback(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+
+    def get_audio_rate(self):
+        return self.audio_rate
+
+    def set_audio_rate(self, audio_rate):
+        self.audio_rate = audio_rate
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.audio_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.audio_rate)
 
 
-def main(top_block_cls=top_block, options=None):
+def main(top_block_cls=tutorial_two, options=None):
 
     from distutils.version import StrictVersion
     if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
